20ae58bfbdcd933da0562fd1ee3930366eccfc13
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-01-17 15:41:17 +0300

    Add function to extract local part of email

---
+++ checkingTheCorrectnessOfTheEmailAddress.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+
+// Function for obtaining the first part of email (to the symbol @)
+std::string getLocalPart(const std::string& email) {
+    std::string localPart;
+    for (char c : email) {
+        if (c == '@') break; // stop if the symbol is found @
+        localPart += c; // Add the symbol to the local part
+    }
+    return localPart;
+}
