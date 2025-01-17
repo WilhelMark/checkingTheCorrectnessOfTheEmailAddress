ba0bb8442ff5c029de06347d143e4d407719756e
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-01-17 16:02:04 +0300

    Add function to extract domain part of email

--- checkingTheCorrectnessOfTheEmailAddress.cpp
+++ checkingTheCorrectnessOfTheEmailAddress.cpp
@@ -10,3 +10,18 @@ std::string getLocalPart(const std::string& email) {
     }
     return localPart;
 }
+
+// Function for obtaining the second part of email (after symbol @)std::string getDomainPart(const std::string& email) {
+    std::string domainPart;
+    bool atFound = false; // flag for checking the presence of a symbol @
+    for (char c : email) {
+        if (c == '@') {
+            atFound = true; // install the flag if the symbol is found @
+            continue; // miss the character @
+        }
+        if (atFound) {
+            domainPart += c; // Add the symbol to the domain part
+        }
+    }
+    return domainPart;
+}
